0a7c45543771e37a034a319a34cb3ecbd3720470
a1700651 <a1700651@10.10.117.5>
2018-08-24 21:15:10 -0300

    view de chat criada

---
+++ src/controller/ChatController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static javafx.application.Application.launch;
+import static javafx.application.ConditionalFeature.FXML;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.shape.Arc;
+
+/**
+ * FXML Controller class
+ *
+ * @author a1700651
+ */
+public class ChatController implements Initializable {
+
+    @FXML private Arc arc;
+    /**
+     * Initializes the controller class.
+     */
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }   
+    
+}
